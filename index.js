777b4b2f52f7bcc562ede7ef7f7a72ea013f3dd6
naxsyambis <naxsyambis@gmail.com>
2025-10-28 14:45:47 +0700

    menjelankan server express dan menapilkan port berjalan

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
